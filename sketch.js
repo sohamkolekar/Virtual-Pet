8eb3e28864fc23945fa08d20ab0d9a7c5eceedc9
sohamkolekar <68347705+sohamkolekar@users.noreply.github.com>
2020-07-20 20:54:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,23 +1,26 @@
 //Create variables here
- var dog,HappyDog,dogimg,dogHappyimg
- var database;
- var foods,foodStock;
+var dog,HappyDog,dogimg,dogHappyimg
+var database;
+var foods,foodStock;
 
 function preload()
-{
-
+  {
+  
   dogimg=loadImage("images/dogImg.png")
+  
   dogHappyimg=loadImage("images/dogImg1.png")
 	//load images here
 }
 
 function setup() {
   
-  database=firebase.database()
+  database=firebase.database();
   console.log(database);
-  createCanvas(500, 500);
-  dog=createSprite(200,200,30,30);
+  createCanvas(700, 700);
+  dog=createSprite(350,350,10,10);
+  dog.scale=0.5; 
   dog.addImage(dogimg)
+  // conso
 
   //getting the value of food remaining
   foodStock=database.ref('Food');
@@ -29,13 +32,21 @@ function setup() {
 function draw() {  
   background(46,139,87)
   textSize(30)
-  text("Press UP_ARROW KEY TO Feed LEO",200,100)
-     
+  fill("blue")
+  text("Press UP_ARROW KEY to Feed LEO",50,50)
+  
   //add styles here
   if(keyWentDown(UP_ARROW)){   
+   // foods=foods-1
     writeStock(foods)
     dog.addImage(dogHappyimg)
+    
   }
+
+  /*if(keyWentDown(DOWN_ARROW)){
+      foods=foods+1
+      writeStock(foods)
+   }*/
 
   drawSprites();
   textSize(25)
@@ -49,8 +60,14 @@ function readStock(data){
 
 //function which writes value of food remaining in database
 function writeStock(x){
-    database.ref('/').update({
-        Food:x
-    })
-  }
+ if(x<=0 ){
+ x=0
+ }
+ else{
+   x=x-1;
+ }
+ database.ref('/').update({
+ Food:x
+ })
+}
 
